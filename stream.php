bb39fbf554fa6f451d506b4a23c63df4b4a138b4
Lucien Immink <lucien.immink@isaac.nl>
2013-08-12 16:51:00 +0200

    Removed music.json from project removed echo of path from streamer

--- stream.php
+++ stream.php
@@ -26,7 +26,6 @@ stream_context_set_default(array(
 
 // replace certain characters in a way the streaming script can still find the file
 $path = str_replace(array(" ", "+", "&"), array("%20", "%2B", "%26"), $_GET["path"]);
-echo $path;
 // TODO: set base url and port in the settings of the player.
 $url = 'http://www.arielext.org:5000/webman/3rdparty/AudioStation/webUI/audio_stream.cgi/0.mp3?action=streaming&songpath=' . $path;
 $headers = get_headers($url, 1);
