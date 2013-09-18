2c5dcaaf56811af7e724b9d341cf61cbf8b9a4f1
unknown <lucien@.(none)>
2013-09-18 20:32:43 +0200

    temp fix for chrome, at least it is playing atm :)

--- js/controllers/PlayerController.js
+++ js/controllers/PlayerController.js
@@ -1,15 +1,16 @@
-jsmusicdb.controller('PlayerController', ['$scope', '$http', 'switchView', '$rootScope' ,'playerService', function ($scope, $http, switchView, $rootScope, playerService) {
-    "use strict";
-    var playerpath = 'proxy/$s/stream.php?path=',
-        audiotag = $('audio').get(0),
-        watching = [$scope.album, $scope.track],
-    sanitize = function (name) {
-      name = name.replace(/\+/g, '%2B');
-      name = name.replace(/\&/g, '%26');
-      name = name.replace(/#/g, '%23');
-      return name;  
+jsmusicdb.controller('PlayerController', ['$scope', '$http', 'switchView', '$rootScope', 'playerService',
+function($scope, $http, switchView, $rootScope, playerService) {"use strict";
+    var playerpath = 'proxy/$s/stream.php?path=', audiotag = $('audio').get(0), watching = [$scope.album, $scope.track], sanitize = function(name) {
+        name = name.replace(/\+/g, '%2B');
+        name = name.replace(/\&/g, '%26');
+        name = name.replace(/#/g, '%23');
+        return name;
     };
-    var play = function (track) {
+    var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+    if (is_chrome) {
+        playerpath = 'proxy/$s/webkit.php?path=';
+    }
+    var play = function(track) {
         $scope.scrobbeld = false;
         if ($scope.track) {
             $scope.track.isPlaying = false;
@@ -18,11 +19,11 @@ jsmusicdb.controller('PlayerController', ['$scope', '$http', 'switchView', '$roo
         $scope.track.isPlaying = true;
         $scope.isPlaying = true;
         $scope.playstate = 'play';
-        
+
         // set audio source
         var src = "";
         if ($rootScope.server !== 0) {
-            src = playerpath.replace('$s', $rootScope.server) + sanitize(track.path) + '&sid='+$rootScope.sid + '&server=' + encodeURIComponent($rootScope.url);
+            src = playerpath.replace('$s', $rootScope.server) + sanitize(track.path) + '&sid=' + $rootScope.sid + '&server=' + encodeURIComponent($rootScope.url);
         } else {
             src = 'file:///' + track.path;
         }
@@ -30,22 +31,22 @@ jsmusicdb.controller('PlayerController', ['$scope', '$http', 'switchView', '$roo
         audiotag.load();
         audiotag.play();
         // another try to fix the damned mobile playback
-        setTimeout(function () {
+        setTimeout(function() {
             audiotag.pause();
             audiotag.play();
         }, 10);
         // set now playling status
         playerService.scrobbleNowPlaying($scope);
     };
-    $scope.$on('playTrack', function (e, track) {
+    $scope.$on('playTrack', function(e, track) {
         // switchView.setAsPlaylist(album);
         play(track);
     });
-    $scope.pos = function () {
+    $scope.pos = function() {
         var percentage = ($scope.position / $scope.len) * 100;
         return (percentage) ? percentage + '%' : '0%';
     };
-    $scope.next = function () {
+    $scope.next = function() {
         var track = playerService.nextTrack($scope.track);
         if (track) {
             play(track);
@@ -56,7 +57,7 @@ jsmusicdb.controller('PlayerController', ['$scope', '$http', 'switchView', '$roo
             audiotag.pause();
         }
     };
-    $scope.previous = function () {
+    $scope.previous = function() {
         var track = playerService.previousTrack($scope.track);
         if (track) {
             play(track);
@@ -68,7 +69,7 @@ jsmusicdb.controller('PlayerController', ['$scope', '$http', 'switchView', '$roo
         }
     };
     $scope.playstate = 'play';
-    $scope.playpause = function () {
+    $scope.playpause = function() {
         if ($scope.playstate === 'play') {
             $scope.playstate = 'pause';
             $scope.track.isPlaying = false;
@@ -79,24 +80,21 @@ jsmusicdb.controller('PlayerController', ['$scope', '$http', 'switchView', '$roo
             audiotag.play();
         }
     };
-    $scope.stop = function () {
+    $scope.stop = function() {
         $scope.playstate = 'play';
         $scope.isPlaying = false;
         $scope.track.isPlaying = false;
         audiotag.pause();
         audiotag.src = '';
     };
-    $scope.updatePosition = function ($event) {
+    $scope.updatePosition = function($event) {
         if ($scope.len) {
-            var clientX = $event.clientX,
-                left = clientX - $($event.target).offset().left,
-                perc = (left / $($event.target).width()),
-                time = perc * $scope.len;
+            var clientX = $event.clientX, left = clientX - $($event.target).offset().left, perc = (left / $($event.target).width()), time = perc * $scope.len;
             audiotag.currentTime = time;
         }
     };
     $scope.muteState = 'up';
-    $scope.toggleMute = function () {
+    $scope.toggleMute = function() {
         if ($scope.muteState === 'up') {
             $scope.muteState = 'off';
             audiotag.volume = 0;
@@ -105,7 +103,7 @@ jsmusicdb.controller('PlayerController', ['$scope', '$http', 'switchView', '$roo
             audiotag.volume = 1;
         }
     };
-    $scope.love = function (album, track) {
+    $scope.love = function(album, track) {
         if (localStorage.getItem("key")) {
             var url = 'http://ws.audioscrobbler.com/2.0/', data = {
                 method : 'track.love',
@@ -113,17 +111,18 @@ jsmusicdb.controller('PlayerController', ['$scope', '$http', 'switchView', '$roo
                 artist : album.Artiest,
                 track : track.Titel,
                 sk : localStorage.getItem("key"),
-                api_sig: lastfm.signplayinglove(album.Artiest, track.Titel, 'track.love')
+                api_sig : lastfm.signplayinglove(album.Artiest, track.Titel, 'track.love')
             };
-            $.post(url, data, function(json) {});
+            $.post(url, data, function(json) {
+            });
         }
     };
-    var scrobble = function () {
+    var scrobble = function() {
         playerService.scrobble($scope);
         $scope.scrobbeld = true;
     };
     $scope.playlistView = 'list';
-    $scope.toggleView = function () {
+    $scope.toggleView = function() {
         $("#main .container > div").hide();
         if ($scope.playlistView === 'list') {
             $("#playlist").show();
@@ -133,9 +132,9 @@ jsmusicdb.controller('PlayerController', ['$scope', '$http', 'switchView', '$roo
             $scope.playlistView = 'list';
         }
     };
-    
+
     $scope.isRandom = 'arrow-right';
-    $scope.toggleRandom = function () {
+    $scope.toggleRandom = function() {
         if ($scope.isRandom === 'arrow-right') {
             playerService.random(true);
             $scope.isRandom = 'random';
@@ -144,9 +143,9 @@ jsmusicdb.controller('PlayerController', ['$scope', '$http', 'switchView', '$roo
             $scope.isRandom = 'arrow-right';
         }
     };
-    
+
     // audiotag events
-    audiotag.addEventListener('timeupdate', function () {
+    audiotag.addEventListener('timeupdate', function() {
         $scope.position = audiotag.currentTime;
         $scope.len = audiotag.duration;
         if ($scope.position / $scope.len > 0.5 && !$scope.scrobbeld) {
@@ -154,7 +153,7 @@ jsmusicdb.controller('PlayerController', ['$scope', '$http', 'switchView', '$roo
         }
         $scope.$apply();
     });
-    audiotag.addEventListener('ended', function () {
+    audiotag.addEventListener('ended', function() {
         $scope.next();
     });
-}]);
+}]); 
