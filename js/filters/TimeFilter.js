69a5c1cf03557cf2225b4e92c3bf9877de5032cc
Lucien Immink <lucien.immink@isaac.nl>
2013-10-03 13:55:17 +0200

    fixed infinity display on mp3's with an unknown length

--- js/filters/TimeFilter.js
+++ js/filters/TimeFilter.js
@@ -6,7 +6,7 @@ angular.module('TimeFilters', []).filter('timeFilter', function() {
 		return n;
 	};
 	return function(total) {
-		if (isNaN(total)) {
+		if (isNaN(total) || total < 1 || total === Infinity) {
 			return null;
 		};
 		var days = parseInt(total / (3600 * 24)), rest = parseInt(total % (3600 * 24)), hours = parseInt(rest / 3600), rest = parseInt(total % 3600), minutes = parseInt(rest / 60), seconds = parseInt(rest % 60);
@@ -28,9 +28,6 @@ angular.module('TimeFilters', []).filter('timeFilter', function() {
 		}
 		if (seconds < 10) {
 			seconds = "0" + seconds;
-		}
-		if (isNaN(seconds) || isNaN(minutes) || isNaN(hours)) {
-			return '';
 		}
 		return days + hours + minutes + ":" + seconds;
 	};
